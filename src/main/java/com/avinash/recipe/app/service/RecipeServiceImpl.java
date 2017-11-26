c2e608267a74abfd2dfbda652d2ed44ce1ae647f
Avinash Tiwari <qwe123kids@gmail.com>
2017-11-26 15:14:57 -0700

    sample work swith H2 Database , Spring and works for reterving the data

---
+++ src/main/java/com/avinash/recipe/app/service/RecipeServiceImpl.java
@@ -0,0 +1,30 @@
+package com.avinash.recipe.app.service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.avinash.recipe.app.domain.Recipe;
+import com.avinash.recipe.app.repositories.RecipeRepository;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+  private final  RecipeRepository recipeRepository;
+  
+  @Autowired
+  public RecipeServiceImpl(final  RecipeRepository recipeRepository){
+	  this.recipeRepository = recipeRepository;
+  }
+  
+  
+	@Override
+	public Set<Recipe> setRecipe() {
+		// TODO Auto-generated method stub
+		Set<Recipe> recipeSet = new HashSet<Recipe>();
+		 recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
+		return recipeSet;
+	}
+
+}
